bf52e413d305fef369731b9c62433e4a5b9200d9
menssajero <willkergaciab@gmail.com>
2023-07-14 15:51:17 -0400

    redux install

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, AppRegistry} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { ConfigureStoreOptions, configureStore } from '@reduxjs/toolkit';
+import { Styles } from './estilos/styles';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={Styles.container}>
+      <Text>prueba de redux</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export const Styles = StyleSheet.create({
+//   container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//   });
