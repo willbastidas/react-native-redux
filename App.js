7d5c0190a03742159b7c5e21ed289a37b9514f38
willbastidas <willkergaciab@gmail.com>
2023-07-17 16:50:49 -0400

    documentando 1

--- App.js
+++ App.js
@@ -6,6 +6,13 @@ import AddItem from './components/AddItem';
 import ItemList from './components/ItemList';
 import { Styles } from './estilos/styles';
 
+{/*lo unico que hace app es mostrar la informacion de las funciones 
+que estan tulizado el redux toolkit que en este caso son: 
+*additem.js
+*itemList.js
+*reducers.js
+*store.js
+*/}
 const App = () => {
   return (
     <Provider store={store}>
